fed5681039c9328b80788d3f6794a8db43f319a4
“Housam <housam.kak20@gmail.com>
2022-10-02 23:00:12 +0300

    Backend => Modals: Added custom primary key names to Message and User

--- backend/app/Models/Message.php
+++ backend/app/Models/Message.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+     /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'message_id';
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+     /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'user_id';
 }
