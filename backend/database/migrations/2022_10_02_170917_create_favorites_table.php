10608cb7936d2ff4b58f638be04ae1940f3db7d0
“Housam <housam.kak20@gmail.com>
2022-10-02 21:05:22 +0300

    Backend => Database: Created and migrated all the database tables

---
+++ backend/database/migrations/2022_10_02_170917_create_favorites_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('admirer_id');
+            $table->foreign('admirer_id')->references('user_id')->on('users');
+            $table->unsignedBigInteger('favorited_id');
+            $table->foreign('favorited_id')->references('user_id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
