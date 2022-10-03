b45d46c1e17a976c6ab9d58f0ce7c82b34bf6af0
“Housam <housam.kak20@gmail.com>
2022-10-03 11:48:35 +0300

    backend => routing: removed optional parameters from signup api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
-// 
-Route::post("/signup/{name?}/{email?}/{age?}/{bio?}/{picture?}/{gender?}/{favorite_gender?}", [LandingController::class, "addorExistingUser"])->name("signup");
+
+Route::post("/signup", [LandingController::class, "addorExistingUser"])->name("signup");
