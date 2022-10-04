f025f6b0134f3f8fd5896062c546d2630b9c8cdd
“Housam <housam.kak20@gmail.com>
2022-10-04 19:49:59 +0300

    backend => jwt: Configured jwt

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,4 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 
 Route::post("/signup", [LandingController::class, "addorExistingUser"])->name("signup");
-Route::post("/signin", [LandingController::class, "signin"])->name("signin");
+
+
+Route::group(['middleware' => 'auth:api',], function () {
+    Route::post("/signin", [AuthController::class, "signin"])->name("signin");
+
+    // Route::post('logout', 'AuthController@logout');
+    // Route::post('refresh', 'AuthController@refresh');
+    // Route::post('me', 'AuthController@me');
+});
