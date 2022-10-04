77b1427b4b27981c9e0e1e096d3de740906c6edc
“Housam <housam.kak20@gmail.com>
2022-10-04 23:09:13 +0300

    backend => Routing: added profile api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,22 @@
 <?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LandingController;
-
-Route::post("/signup", [LandingController::class, "addorExistingUser"])->name("signup");
-
-// jwt didn't work it always showed an error, so I needed to drop it and continue working
+// Note:
+// // jwt didn't work it always showed an error, so I needed to drop it and continue working
 // I had the following error
 //  - Root composer.json requires tymon/jwt-auth ^0.5.12 -> satisfiable by tymon/jwt-auth[0.5.12].
 // - tymon/jwt-auth 0.5.12 requires illuminate/support ~5.0 -> found illuminate/support[v5.0.0, ..., 5.8.x-dev] 
 // but these were not loaded, likely because it conflicts with another require.
-
-
-
 // Route::group(['middleware' => 'auth:api',], function () { 
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\ProfileController;
+
+Route::post("/signup", [LandingController::class, "addorExistingUser"])->name("signup");
 Route::post("/signin", [LandingController::class, "signin"])->name("signin");
+
+Route::post("/user/info", [ProfileController::class, "userInfo"])->name("userInfo");
+Route::post("/user/update", [ProfileController::class, "userUpdate"])->name("userUpdate");
+
+
 // });
