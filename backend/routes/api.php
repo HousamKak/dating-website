f84439a961e4933b8239b983092ab5c62abbdb82
“Housam <housam.kak20@gmail.com>
2022-10-04 20:44:15 +0300

    backend => jwt: Removed jwt because it didn't work

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\LandingController;
 
 Route::post("/signup", [LandingController::class, "addorExistingUser"])->name("signup");
 
+// jwt didn't work it always showed an error, so I needed to drop it and continue working
+// I had the following error
+//  - Root composer.json requires tymon/jwt-auth ^0.5.12 -> satisfiable by tymon/jwt-auth[0.5.12].
+// - tymon/jwt-auth 0.5.12 requires illuminate/support ~5.0 -> found illuminate/support[v5.0.0, ..., 5.8.x-dev] 
+// but these were not loaded, likely because it conflicts with another require.
 
-Route::group(['middleware' => 'auth:api',], function () {
-    Route::post("/signin", [AuthController::class, "signin"])->name("signin");
 
-    // Route::post('logout', 'AuthController@logout');
-    // Route::post('refresh', 'AuthController@refresh');
-    // Route::post('me', 'AuthController@me');
-});
+
+// Route::group(['middleware' => 'auth:api',], function () { 
+Route::post("/signin", [LandingController::class, "signin"])->name("signin");
+// });
