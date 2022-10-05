5fe7309278e3a05a301b0af8914263ec778fff21
“Housam <housam.kak20@gmail.com>
2022-10-05 21:41:05 +0300

    backend => adding favorites: added favorited to database

--- backend/app/Http/Controllers/DisplayController.php
+++ backend/app/Http/Controllers/DisplayController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Favorite;
 
 class DisplayController extends Controller
 {
@@ -15,5 +16,23 @@ class DisplayController extends Controller
             "status" => "Success",
             "result" => $users
         ]);
+    }
+
+    function addFavorites(Request $request)
+    {
+
+        $fav = ['admirer_id' => $request->admirer_id, 'favorited_id' => $request->favorited_id];
+        $fav_relation = Favorite::where($fav)->get();
+
+        if (count($fav_relation) == 0) {
+            $faved_people = new Favorite;
+            $faved_people->admirer_id = $request->admirer_id;
+            $faved_people->favorited_id = $request->favorited_id;
+            $faved_people->save();
+            return response()->json([
+                "status" => "Success",
+                "result" => $faved_people
+            ]);
+        }
     }
 }
