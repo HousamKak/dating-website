22af8ff91cca29b305030af38111a599a8006c0f
“Housam <housam.kak20@gmail.com>
2022-10-05 20:12:39 +0300

    backend => feed api: corrected the receiving gender

--- backend/app/Http/Controllers/DisplayController.php
+++ backend/app/Http/Controllers/DisplayController.php
@@ -9,7 +9,7 @@ class DisplayController extends Controller
 {
     function getFeed(Request $request)
     {
-        $users = USER::select("user_id", "picture", "name", "age", "gender", "bio", "location")->where("favorite_gender", $request->favorite_gender)->get();
+        $users = USER::select("user_id", "picture", "name", "age", "gender", "bio", "location")->where("gender", $request->favorite_gender)->get();
 
         return response()->json([
             "status" => "Success",
